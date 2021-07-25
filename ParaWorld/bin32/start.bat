268cbe346d3135935ef27b80923283668a308f6e
CF <375179330@qq.com>
2021-07-25 21:54:12 +0800

    test demo

---
+++ ParaWorld/bin32/start.bat
@@ -0,0 +1,4 @@
+call "ParaEngineClient_d.exe" bootstrapper="main.lua" single="false" mc="true" noupdate="true" dev="%~dp0" 
+
+@REM here some basic example that you can run
+@REM https://github.com/LiXizhi/ParaCraftSDK/blob/master/samples/1.%20HelloWorld/source/HelloWorld/main.lua
